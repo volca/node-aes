e1147a5b2df81b99a83e523b6098d75d97c562cf
yanc <volcas@gmail.com>
2020-06-16 16:28:52 +0800

    add encrypt example

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ decipher = crypto.createDecipheriv("aes-128-ecb", key, '')
 decipher.setAutoPadding(false)
 result = decipher.update(encrypted).toString('hex');
 result += decipher.final().toString('hex');
+decryptedHex = result.toUpperCase();
 
 var tmp = result.substring(0, 12);
 var mac = tmp.match(/.{1,2}/g).reverse().join("");
@@ -17,3 +18,11 @@ console.log("result\t: " + result);
 console.log("mac\t: " + mac);
 console.log("temp\t: " + result.substring(14, 18));
 console.log("hum\t: " + result.substring(18, 22));
+
+// encrypt data
+var newDecrypted = Buffer.from(decryptedHex,'hex');
+cipher = crypto.createCipheriv("aes-128-ecb", key, '')
+cipher.setAutoPadding(false)
+result = cipher.update(newDecrypted).toString('hex');
+result += cipher.final().toString('hex');
+console.log("enc\t: " + result);
